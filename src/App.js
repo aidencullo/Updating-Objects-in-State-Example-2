126262d077bdba9da40bf3474b6ec61df2d8f5ee
Aiden Cullo <aidencullo@gmail.com>
2024-09-19 14:08:26 -0400

    set to new obj

--- src/App.js
+++ src/App.js
@@ -8,34 +8,43 @@ export default function Form() {
   });
 
   function handleFirstNameChange(e) {
-    person.firstName = e.target.value;
+    setPerson({
+      ...person,
+      firstName: e.target.value
+    });
   }
 
   function handleLastNameChange(e) {
-    person.lastName = e.target.value;
+    setPerson({
+      ...person,
+      lastName: e.target.value
+    });
   }
 
   function handleEmailChange(e) {
-    person.email = e.target.value;
+    setPerson({
+      ...person,
+      email: e.target.value
+    });
   }
 
   return (
-    <>
+      <>
       <label>
-        First name:
-        <input value={person.firstName} onChange={handleFirstNameChange} />
+      First name:
+      <input value={person.firstName} onChange={handleFirstNameChange} />
       </label>
       <label>
-        Last name:
-        <input value={person.lastName} onChange={handleLastNameChange} />
+      Last name:
+      <input value={person.lastName} onChange={handleLastNameChange} />
       </label>
       <label>
-        Email:
-        <input value={person.email} onChange={handleEmailChange} />
+      Email:
+      <input value={person.email} onChange={handleEmailChange} />
       </label>
       <p>
-        {person.firstName} {person.lastName} ({person.email})
-      </p>
-    </>
+      {person.firstName} {person.lastName} ({person.email})
+    </p>
+      </>
   );
 }
